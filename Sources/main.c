dfd49cc98a39817a104a1a448790f7f3b7961673
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-06-06 15:49:47 +0330

    start-a-project

---
+++ Sources/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "../Headers/other.h"
+
+int main(void) 
+{ 
+    printf("%d\n", getfavoritenumber()); 
+
+    return 0;
+}
---
+++ Sources/other.c
@@ -0,0 +1,6 @@
+#include "../Headers/other.h"
+
+int getfavoritenumber(void)
+{
+    return 3;
+}

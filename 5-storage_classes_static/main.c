1c009454cba69c0269e329bfb9043f2b3b8a6aeb
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-06-09 19:59:45 +0330

    5

--- 5-storage_classes_static/main.c
+++ 5-storage_classes_static/main.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 
+int initializer() { return 100; }
+
 int fun() {
   static int count = 0;
 
+  static int initialized;
+
+  initialized = initializer();
+
+  static int hasValue = 50;
+
+  static int noValue;
+
+  noValue = 51;
+
+  int localVar = 0;
+
+  printf("automatic = %d \t static = %d\n", localVar, count);
+
+  printf("hasValue = %d \t noValue = %d\n", hasValue, noValue);
+
+  printf("initialized = %d\n", initialized);
+
   count++;
+  localVar++;
+  hasValue++;
+  noValue++;
+  initialized++;
 
   return count;
 }
 
 int main() {
-  printf("%d\n", fun());
 
   //! the static variable
   //! doesn't redecalre each time it's called,
-  //!  it just reassigned
+  //! it just reassigned
   //! so, in this example, it holds 1 and then add 1 to it
-  printf("%d\n", fun());
+
+
+  //? if the static variable doesn't assign directly,
+  //? the default 0 will be push to it.
+  //? so everytime it's called, will be initialized 0 by the compiler;
+
+  for (int i = 0; i < 5; i++) {
+    fun();
+  }
 
   return 0;
 }

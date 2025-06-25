72dfe499543e58711f9f0a0aa9a791ff4afda677
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-06-25 12:01:39 +0330

    12-I/O-string-functions

--- 12-Input_Output/string-functions.c
+++ 12-Input_Output/string-functions.c
@@ -26,29 +26,49 @@ printf("Our Buffer contains : %s\n", buf);
 */
 
   //! ---2---
-  char buffer[32];
-  char *b = buffer;
-  size_t bufsize = 32;
-  size_t characters;
-
-  /*
-  //? if we had : char *buffer = NULL;
-  //? we could define it as below :
-  buffer = (char *)malloc(bufsize * sizeof(char));
   
-  if(buffer == NULL){
+  // char buffer[32];
+  // char *b = buffer;
+  // size_t bufsize = 32;
+  // size_t characters;
+
+  // /*
+  // //? if we had : char *buffer = NULL;
+  // //? we could define it as below :
+  // buffer = (char *)malloc(bufsize * sizeof(char));
+  
+  // if(buffer == NULL){
+  //   exit(1);
+  // }
+
+  // then the character sould be :
+  // getline(&buffer, &bufsize, stdin);
+  // */
+
+  // printf("Type something : ");
+  // characters = getline(&b, &bufsize, stdin);
+
+  // printf("%zu characters were read.\n", characters);
+  // printf("You typed : \n\t\t%s\n", buffer);
+ 
+  //! ---3---
+
+  //? int fputs(const char * buffer, FILE *filePointer)
+  char string[40];
+  strcpy(string, "Hello world !");
+  puts(string);
+
+  FILE *fp = NULL;
+  fp = fopen("file.txt", "w");
+
+  if(fp == NULL){
     exit(1);
   }
 
-  then the character sould be :
-  getline(&buffer, &bufsize, stdin);
-  */
+  fputs("Hello There, I hope this article will help !", fp);
 
-  printf("Type something : ");
-  characters = getline(&b, &bufsize, stdin);
+  fclose(fp);
 
-  printf("%zu characters were read.\n", characters);
-  printf("You typed : \n\t\t%s\n", buffer);
-
+  //? the file.txt containing that sentence is now available in directory
   return 0;
 }

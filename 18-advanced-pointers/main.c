773ba6ae621a1370dd4ce68093e4d2367e73d317
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-07-03 11:29:49 +0330

    18-advanced-pointers

--- 18-advanced-pointers/main.c
+++ 18-advanced-pointers/main.c
@@ -1,15 +1,20 @@
 #include <malloc.h>
 #include <stdio.h>
+#include <string.h>
 
 void foo(int *ptr);
 void hoo(int *ptr);
+void goo(int **ptr);
+void joo(char *ptr);
 
 int main() {
   int *ptr = NULL;
+  char *ptr_2 = NULL; //* to use in joo()
 
   ptr = (int *)malloc(sizeof(int));
 
   *ptr = 10;
+
 
   foo(ptr);
   printf("in foo %d\n\n\n" , *ptr);
@@ -24,6 +29,34 @@ int main() {
   //? as you can see, by using double pointers,
   //? we can easily modify the ptr now .
 
+
+  // goo(&ptr);
+  // printf("in goo %d\n\n\n" , *ptr); //* returns 5
+  
+  joo(ptr_2);
+  printf("in joo %s\n", ptr_2);
+  //? after we run the program, it crashes.
+  //? why? because we are allocating memory to a copy,
+  //? this is the point we should use p-o-p !
+  //* see the details here :
+  //* we define a pointer called ptr,
+  //* ptr = 0xffffff ;
+  //* this address, pointing to a value,
+  //* 0xffffff = 6 ; (for example)
+  //* we also have the address of ptr, 0xjjjjjj
+  //* &ptr = 0xjjjjjj ;
+  //* if we make a copy of this pointer -> ptr_1,
+  //* we will have :
+  //* a new address : &ptr_1 = 0xkkkkkk ;
+  //* ptr_1 = 0xffffff ; (still pointing to the same address)
+  //* so if we try to allocate memory to "ptr's address",
+  //* we are allocating memory to 0xkkkkkk not 0xjjjjjj
+  //* we are allocating memory to  &ptr_1  not  &ptr .
+  //* so we can't change the value of what ptr_1 is for a malloc,
+  //* inside a function, unless we pass-in a double-pointer,
+  //* because we can't allocate memory to a copy of a pointer.
+  free(ptr_2);
+
   return 0;
 }
 
@@ -35,3 +68,12 @@ void hoo(int *ptr) {
   int a = 5;
   *ptr = a;
 }
+void goo(int **ptr) {
+  int a = 5;
+  *ptr = &a;
+}
+void joo(char *ptr){
+  ptr = malloc(255);
+  strcpy(ptr, "hello world\n");
+}
+

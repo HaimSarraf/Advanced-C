eaadf79c2822a41630ad986d03bf34988183396b
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-07-03 11:59:03 +0330

    18-advanced-pointers-chalenge

---
+++ 18-advanced-pointers/challenge-double-pointers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 123;
+    int *ptr = &num;
+    int **pptr = &ptr;
+
+
+    printf("The value of num is : %d\n" , num);
+    printf("The value of num using ptr is : %d\n" , *ptr);
+    printf("The value of num using pptr is : %d\n\n" , **pptr);
+
+    printf("Address of num is            : %p\n", &num);
+    printf("Address of num using ptr is  : %p\n", ptr);
+    printf("Address of num using pptr is : %p\n", *pptr);
+
+    return 0;
+}

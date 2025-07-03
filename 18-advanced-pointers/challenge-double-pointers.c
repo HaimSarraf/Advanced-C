b52094cde27ffda68e2d7b4eed332f619be0ea42
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-07-03 12:27:19 +0330

    18-advanced-pointers-chalenge

--- 18-advanced-pointers/challenge-double-pointers.c
+++ 18-advanced-pointers/challenge-double-pointers.c
@@ -3,20 +3,28 @@
 int main()
 {
     int num = 123;
-    int *ptr = &num;
-    int **pptr = &ptr;
+    int *singlePointer = NULL;
+    int **doublePointer = NULL;
+
+    singlePointer = &num;
+    doublePointer = &singlePointer;
 
 
-    printf("The value of num is            : %d\n" , num);
-    printf("The value of num using ptr is  : %d\n" , *ptr);
-    printf("The value of num using pptr is : %d\n\n" , **pptr);
+    printf("The value of num is                                    : %d\n" , num);
+    printf("The value of num using singlePointer is                : %d\n" , *singlePointer);
+    printf("The value of num using doublePointer is                : %d\n\n" , **doublePointer);
 
-    printf("Address of num is              : %p\n", &num);
-    printf("Address of num using ptr is    : %p\n", ptr);
-    printf("Address of num using pptr is   : %p\n", *pptr);
+    printf("Address of num is                                      : %p\n", &num);
+    printf("Address of num using singlePointer is                  : %p\n", singlePointer);
+    printf("Address of num using doublePointer is                  : %p\n\n", *doublePointer);
 
-    printf("Value of Pointer ptr is        : %p\n", &ptr);
-    printf("Value of Pointer pptr is       : %p\n", pptr);
+    printf("Value of Pointer singlePointer is                      : %p\n", singlePointer);
+    printf("Value of Pointer singlePointer using doublePointer is  : %p\n\n", *doublePointer);
+    printf("Address of Pointer singlePointer is                    : %p\n" , &singlePointer);
+    printf("Address of Pointer singlePointer using doublePointer is: %p\n\n" , doublePointer);
 
+    printf(" Value  of Pointer doublePointer is                    : %p\n", doublePointer);
+    printf("Address of Pointer doublePointer is                    : %p\n", &doublePointer);
+    
     return 0;
 }

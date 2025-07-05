a93c423bb2e4b53691e89126ec326e05a4b1b5e0
Haim Sarraf <sarhangaurelianobuendia@yahoo.com>
2025-07-05 10:44:35 +0330

    19-static_libraries

---
+++ 19-static-libraries/Testing/Sources/main.c
@@ -0,0 +1,9 @@
+#include "./../../../19-static-libraries/myLib/Headers/myLib.h"
+
+
+int main()
+{
+    print();
+
+    return 0;
+}
---
+++ 19-static-libraries/myLib/Sources/myLib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print(void)
+{
+    printf("\n\n\nprint() called from a static library\n\n\n");
+}
